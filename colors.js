a09b74767cb75391d1299bff1002f253a08e9961
kun6685 <107320386+kun6685@users.noreply.github.com>
2022-06-30 03:08:40 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,70 @@
+var Links = {
+  LinkSetColor(color) {
+    //var alist = document.querySelectorAll('a');
+    //var i = 0;
+    //while(i<alist.length) {
+      //alist[i].style.color = color;
+      //i++;
+    //}
+    $('a').css('color', color);
+  }
+}
+var Body = {
+  BodySetColor : function (color) {
+  //document.querySelector('body').style.color = color;
+  $('body').css('color', color);
+},
+
+  BackgroundSetColor : function (color) {
+  //document.querySelector('body').style.backgroundColor = color;
+  $('body').css('backgroundColor', color);  
+}
+}
+
+var BtnHandler1 = {
+  BtnColor : function(color) {
+    document.querySelector('.btn').style.color = color;
+    document.querySelector('.btn').style.border = color;
+  }
+  
+}
+
+var BtnHandler2 = {
+ BtnBorder : function(style) {
+  document.querySelector('.btn').style.border = style;
+ } 
+}
+
+function handler(self) {
+var target = document.querySelector('body');
+
+if(self.value === 'night') {
+Body.BackgroundSetColor('black');
+Body.BodySetColor('white');
+self.value = 'day';
+Links.LinkSetColor('white');
+BtnHandler1.BtnColor('white');
+BtnHandler2.BtnBorder('1px solid white');
+document.querySelector('h1').style.borderBottom = '1px solid white';
+document.querySelector('#grid ol').style.borderRight = '1px solid white';
+if(matchMedia("screen and (max-width: 800px)").matches && (self.value === 'night' || self.value === 'day') ){
+  document.querySelector('h1').style.border = 'none';
+  document.querySelector('#grid ol').style.border = 'none';
+  }
+}
+else {
+Body.BackgroundSetColor('white');
+Body.BodySetColor('black');
+self.value = 'night';
+Links.LinkSetColor('black');
+BtnHandler1.BtnColor('black');
+BtnHandler2.BtnBorder('1px solid black');
+document.querySelector('h1').style.borderBottom = '1px solid black';
+document.querySelector('#grid ol').style.borderRight = '1px solid black';
+  }
+}
+
+
+
+
+
